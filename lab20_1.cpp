bacccb25925d963ac954ff92a924c75b87cd9b53
wasiree <wasiree098@gmail.com>
2023-02-25 21:38:06 +0700

    ok

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -1,12 +1,18 @@
 #include<iostream>
 using namespace std;
+#include<cmath>
 
 struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
+double overlap(Rect a, Rect b){
+    double A1w,A2w,jk;
+    A1w = min(a.x+a.w,b.x+b.w) - max(a.x,b.x);
+    A2w = min(b.y,a.y) - max(a.y-a.h,b.y-b.h);
+    jk = A1w*A2w;
+    if(jk < 0) jk = 0;
+    
+    return jk;
 
 }
